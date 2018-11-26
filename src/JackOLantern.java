18f0d7b2649b854c34881241aae686b7854e5136
BT_1N3_03@BKTECH.LOCAL <BT_1N3_03@BKTECH.LOCAL>
2018-11-26 14:45:53 -0500

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,36 @@
+public class JackOLantern
+{
+    String [][] faceFeatures;
+    public  JackOLantern (String [][] faceFeatures)
+    {
+        this.faceFeatures = faceFeatures;
+    }
+    public void fill(String str)
+    {
+        for(int i = 0; i < faceFeatures.length; i++)
+        {
+            for (int n = 0; i < faceFeatures[i].length; n++ )
+            {
+                faceFeatures [i][n] = str;
+            }
+        }
+    }
+    public String toString ()
+    {
+        String output = "";
+        for (int i = 0; i < faceFeatures.length; i++)
+        {
+            for (int n = 0; n <faceFeatures [i].length; n++)
+            {
+                output = faceFeatures[i] [n] + "\n";
+            }
+        }
+        return output;
+    }
+    public void edit (String replace, int row, int column)
+    {
+        
+    }
+
+
+}
